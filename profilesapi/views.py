4458ae5e0456a8e0ca5e994ca1293e8db5f5a36e
Mildness <m.akomoize@gmail.com>
2022-06-25 01:39:40 +0100

    created http request methods

--- profilesapi/views.py
+++ profilesapi/views.py
@@ -1,8 +1,11 @@
 from urllib import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profilesapi import serializers
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
     def get(self, request, format=None):
         an_apiview = [
             "Possesses the http methods (get, post, patch, put, delete)",
@@ -12,3 +15,22 @@ class HelloApiView(APIView):
         ]
         
         return Response({'message': 'hello', "an_apiview": an_apiview})
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Welcome {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+        
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+    
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+    
+    def delete(self, request, pk=None):
+        return Response({'method':'DELETE'})
