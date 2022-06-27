7067dff46cb89ac457d8c5befb1cddc3a002d345
Mildness <m.akomoize@gmail.com>
2022-06-27 02:08:50 +0100

    added search filter

--- profilesapi/views.py
+++ profilesapi/views.py
@@ -1,7 +1,8 @@
+from encodings import search_function
 from urllib import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, filters
 from profilesapi import serializers, models
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
@@ -13,3 +14,5 @@ class UserProfileViewset(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
