af2ba53ac134ffe32db7e47d9946c8276355c460
Mildness <m.akomoize@gmail.com>
2022-06-27 01:55:00 +0100

    created profile serializers and viewsets

--- profilesapi/urls.py
+++ profilesapi/urls.py
@@ -1,13 +1,17 @@
+from email.mime import base
 from django.db import router
 from django.urls import path, include
 from profilesapi import views
 from rest_framework.routers import DefaultRouter
 
 
+
+
 router = DefaultRouter()
-router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewset)
 
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view()),
+    # path('hello-view/', views.HelloApiView.as_view()),
+    # path('', include(router.urls))
     path('', include(router.urls))
 ]
--- profilesapi/views.py
+++ profilesapi/views.py
@@ -2,70 +2,14 @@ from urllib import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profilesapi import serializers
+from profilesapi import serializers, models
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from profilesapi import permissions
 
-class HelloApiView(APIView):
-    serializer_class = serializers.HelloSerializer
-    def get(self, request, format=None):
-        an_apiview = [
-            "Possesses the http methods (get, post, patch, put, delete)",
-            "Is similar to Django View",
-            "Gives you most control over application logic",
-            "Is mapped manually to URLS",
-        ]
-        
-        return Response({'message': 'hello', "an_apiview": an_apiview})
-    
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        
-        if serializer.is_valid():
-            name = serializer.validated_data.get('name')
-            message = f'Welcome {name}'
-            return Response({'message': message})
-        else:
-            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-        
-    def put(self, request, pk=None):
-        return Response({'method': 'PUT'})
-    
-    def patch(self, request, pk=None):
-        return Response({'method': 'PATCH'})
-    
-    def delete(self, request, pk=None):
-        return Response({'method':'DELETE'})
-    
-    
-class HelloViewSet(viewsets.ViewSet):
-    serializer_class = serializers.HelloSerializer
-    
-    def list(self, request):
-        a_viewset = [
-            'uses action CRUD',
-            'maps to urls through routers',
-            'more functionality with less code'
-        ]
-        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
-    
-    def create(self, request):
-        serializer = self.serializer_class(data=request.data)
-        
-        if serializer.is_valid():
-            name = serializer.validated_data.get('name')
-            message = f'Welcome {name}!'
-            return Response({'message': message})
-        else:
-            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-        
-    def retrieve(self, request, pk=None):
-        return Response({'http_method': 'GET'})
-    
-    def update(self, request, pk=None):
-        return Response({'http_method':'PUT'})
-    
-    def partial_update(self, request, pk=None):
-        return Response({'http_method':'PATCH'})
-    
-    def destroy(self, request, pk=None):
-        return Response({'http_method':'DELETE'})
+
+class UserProfileViewset(viewsets.ModelViewSet):
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
