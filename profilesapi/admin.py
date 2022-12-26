414bf277bedfd072a05de1d1bd24c72345563af5
Mildness Akomoize <66227187+Mildness10@users.noreply.github.com>
2022-12-25 22:57:58 -0500

    Update admin.py

--- profilesapi/admin.py
+++ profilesapi/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import ProfileFeedItem, UserProfile
 
-
+#REGISTERING OF MODELS IN ADMIN SITE
 admin.site.register(UserProfile)
 admin.site.register(ProfileFeedItem)
